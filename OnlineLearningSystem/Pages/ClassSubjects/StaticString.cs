23acd6d7934c4a7373e55637024d13b4de41be79
hieu <kininmontal2003@gmail.com>
2024-07-21 15:28:18 +0700

    Phan cua hieu merge lan 1

---
+++ OnlineLearningSystem/Pages/ClassSubjects/StaticString.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineLearningSystem.Pages.ClassSubjects
+{
+    public static class StaticString
+    {
+        //role name
+        public static string StringRoleTeacher = "Teacher";
+        //error
+        public static string THIS_SUBJECT_EXIST = "THIS SUBJECT ALREADY ADD TO";
+        
+    }
+}
